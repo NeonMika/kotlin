d6db942789bb139eb0aa5a4de7b4f60b885f237a
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2024-07-31 10:41:35 +0300

    [Test] Mute some JS tests
    Two of them actually shouldn't run on JS
    The issue for the last one is KT-70393

--- compiler/testData/loadJava/compiledKotlin/annotations/types/SupertypesAndBounds.kt
+++ compiler/testData/loadJava/compiledKotlin/annotations/types/SupertypesAndBounds.kt
@@ -1,6 +1,7 @@
 // PLATFORM_DEPENDANT_METADATA
-//ALLOW_AST_ACCESS
-
+// ALLOW_AST_ACCESS
+// IGNORE_BACKEND: JS_IR
+// Reason: java.io.Serializable shouldn't be accessible in JS
 package test
 
 import java.io.Serializable
--- compiler/testData/loadJava/compiledKotlin/fromLoadJava/kotlinSignature/AllBoundsInWhen.kt
+++ compiler/testData/loadJava/compiledKotlin/fromLoadJava/kotlinSignature/AllBoundsInWhen.kt
@@ -1,4 +1,7 @@
 // PLATFORM_DEPENDANT_METADATA
+// IGNORE_BACKEND: JS_IR
+// Reason: java.io.Serializable shouldn't be accessible in JS
+
 package test
 
 import java.io.Serializable
