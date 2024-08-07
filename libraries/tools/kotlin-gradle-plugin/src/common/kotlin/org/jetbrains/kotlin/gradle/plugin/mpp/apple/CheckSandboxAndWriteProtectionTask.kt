9b990a124fba79057371b8df0afabceef88a953f
Andrey Yastrebov <andrey.yastrebov@jetbrains.com>
2024-08-07 17:40:15 +0200

    fixup! fixup! fixup! fixup! fixup! KT-68572 Integrate Swift Export DSL into existing pipeline

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/apple/CheckSandboxAndWriteProtectionTask.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/apple/CheckSandboxAndWriteProtectionTask.kt
@@ -8,16 +8,19 @@ package org.jetbrains.kotlin.gradle.plugin.mpp.apple
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.*
 import org.gradle.work.DisableCachingByDefault
 import org.jetbrains.kotlin.gradle.plugin.diagnostics.KotlinToolingDiagnostics
+import org.jetbrains.kotlin.gradle.plugin.diagnostics.KotlinToolingDiagnosticsCollector
+import org.jetbrains.kotlin.gradle.plugin.diagnostics.UsesKotlinToolingDiagnostics
 import org.jetbrains.kotlin.gradle.plugin.diagnostics.reportDiagnosticOncePerBuild
 import java.io.File
 import java.io.IOException
 import java.nio.file.Files
 
 @DisableCachingByDefault(because = "We are checking only file permissions")
-internal abstract class CheckSandboxAndWriteProtectionTask : DefaultTask() {
+internal abstract class CheckSandboxAndWriteProtectionTask : DefaultTask(), UsesKotlinToolingDiagnostics {
 
     @get:InputFiles
     @get:PathSensitive(PathSensitivity.RELATIVE)
@@ -36,14 +39,14 @@ internal abstract class CheckSandboxAndWriteProtectionTask : DefaultTask() {
         val sandboxingEnabled = userScriptSandboxingEnabled.get()
 
         when (dirAccessible) {
-            DirAccessibility.NOT_ACCESSIBLE -> project.fireSandboxException(
+            DirAccessibility.NOT_ACCESSIBLE -> fireSandboxException(
                 task,
                 sandboxingEnabled
             )
             DirAccessibility.DOES_NOT_EXIST,
             DirAccessibility.ACCESSIBLE,
                 -> if (sandboxingEnabled) {
-                project.fireSandboxException(task, true)
+                fireSandboxException(task, true)
             }
         }
     }
@@ -70,13 +73,13 @@ internal abstract class CheckSandboxAndWriteProtectionTask : DefaultTask() {
             DirAccessibility.DOES_NOT_EXIST
         }
     }
-}
 
-private fun Project.fireSandboxException(taskName: String, userScriptSandboxingEnabled: Boolean) {
-    reportDiagnosticOncePerBuild(
-        KotlinToolingDiagnostics.XcodeUserScriptSandboxingDiagnostic(
-            taskName,
-            userScriptSandboxingEnabled
+    private fun fireSandboxException(taskName: String, userScriptSandboxingEnabled: Boolean) {
+        toolingDiagnosticsCollector.get().report(
+            this, KotlinToolingDiagnostics.XcodeUserScriptSandboxingDiagnostic(
+                taskName,
+                userScriptSandboxingEnabled
+            )
         )
-    )
+    }
 }
